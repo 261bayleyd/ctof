cc57bd0f4648f9c1e756ae799cdeed7ded509370
261bayleyd <261bayley@gmail.com>
2024-12-06 17:49:16 +0000

    spelling mistake

--- script.js
+++
@@ -1,6 +0,0 @@
-function run(){
-  let eoinput = document.getElementById("inputuser").value
-  let far = eoinput*1.8+32 +" Farhenheit"
-      console.log(far)
-  document.getElementById("output").innerHTML=far
-}
